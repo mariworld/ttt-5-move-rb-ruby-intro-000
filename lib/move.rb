82a89aa48febdc181566bc5bd2965568b37a2f04
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-16 19:56:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,6 +9,8 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(user_input)
+    puts "#{user_input.to_i}"
+end
 #     converted_input = user_input.to_i - 1
 # end
 # puts "Welcome to Tic Tac Toe!"
