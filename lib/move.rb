5d721ed1289fdfecde6a024b57833d9df05ff92e
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-16 19:13:07 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,5 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(position)
-    puts "Choose a spot, 1-9."
-    position = gets.to_i - 1
-end
+def input_to_index(user_input)
+    converted_input = user_input.to_i
